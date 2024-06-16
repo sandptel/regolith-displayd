a2990911df1148373692e643e8796f8c7b3fbc87
sandptel <sandeeppatel626735@gmail.com>
2024-06-16 05:53:22 +0530

    added a flake.nix file

---
+++ default.nix
@@ -0,0 +1,20 @@
+{pkgs ? import <nixpkgs> {}}:
+
+pkgs.rustPlatform.buildRustPackage rec {
+pname = "regolith-displayd";
+version = "main";
+
+src = ./.;
+
+cargoHash = "sha256-JG4tUeeMyBzoUwatBKsZTN3+QFRadSdmH7zrNw9tO7w=";
+
+nativeBuildInputs = with pkgs;[pkg-config rustc];
+buildInputs = with pkgs;[glib];
+
+  meta = {
+    mainProgram = "regolith-displayd";
+    description = "Daemon for providing gnome-control-center DisplayConfig DBus bindings for sway. ";
+    homepage = "https://github.com/regolith-linux/regolith-displayd";
+    license = pkgs.lib.licenses.gpl3Plus;
+  };
+}
